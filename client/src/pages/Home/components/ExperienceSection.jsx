7990448cf9d804c733039ea19ecb4856474b076c
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-04-21 17:43:26 +0800

    style(client/src/pages/Home/components/ExperienceSection.jsx): Add top margin to ExperienceSection
    - Added `mt-16` class to the `div` with class name `${styles.experienceSection} py-16` in `ExperienceSection.jsx` to add a top margin.

--- client/src/pages/Home/components/ExperienceSection.jsx
+++ client/src/pages/Home/components/ExperienceSection.jsx
@@ -2,7 +2,7 @@ import styles from "./ExperienceSection.module.scss"
 
 const ExperienceSection = () => {
     return (
-        <div className={`${styles.experienceSection} py-16`}>
+        <div className={`${styles.experienceSection} py-16 mt-16`}>
 
             <div className={`grid gap-4 mb-10 container mx-auto px-4`}>
                 <h2 className={`text-6xl text-center font-bold`}>LEAD TO EXPERINCE!</h2>
