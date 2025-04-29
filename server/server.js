b6e8a080b7a7d90d091bf4aad9c70d9e41466864
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-04-29 20:36:58 +0800

    Revert "backend/frontend setup"
    This reverts commit 41aaaab995524927031b373f2cd7edafb617ae05.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,6 @@
-const express = require("express");
-const app = express();
+const app = require('./app');
 const PORT = process.env.PORT || 5000;
-const routes = require("./routes/routes.js");
-const db = require("./config/db");
-
-app.use(express.json());
-
-app.get("/ping-db", async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT 1");
-    res.send("Database connection successful!");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-    res.status(500).send("Database connection failed");
-  }
-});
-
-app.use("/api/data", routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
